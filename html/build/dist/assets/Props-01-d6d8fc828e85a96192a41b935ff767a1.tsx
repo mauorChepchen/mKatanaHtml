a4dfb8267e6e6ec486be8a9ad71282f671e639c2
mauorChepchen <chepelev170608@mail.ru>
2024-11-25 20:32:34 +0300

    innit

---
+++ html/build/dist/assets/Props-01-d6d8fc828e85a96192a41b935ff767a1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Props-01" tilewidth="16" tileheight="16" tilecount="112" columns="8">
+ <image source="Props-01.png" width="128" height="224"/>
+</tileset>
